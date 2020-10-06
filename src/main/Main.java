c27254b226acb4818fe45f8bf7853da38b9109df
Lucas-Garavaglia <lucasgrafimar@gmail.com>
2020-10-06 11:30:25 -0300

    Correção de identação e erros no ccodigo

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,34 +7,31 @@ import sjf.*;
 
 public class Main {
 
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+    int entrada;
+    Sjf sjf = new Sjf();
+    RoundRobin rr = new RoundRobin();
 
-    public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
-        boolean loop = true;
-        int entrada;
-        String path = "";
-        Sjf sjf = new Sjf();
-        RoundRobin rr = new RoundRobin();
-
-        System.out.println("[1] Processo não preemptivo");
-        System.out.println("[2] Processo preemptivo");
-        System.out.println("[3] Sair");
-        entrada = ler.nextInt();
-        String pathFile = ler.next();
-        System.out.println(pathFile);
-        if (entrada == 1) {
-            try {
-                sjf.executar(pathFile);
-            } catch (Exception e) {
-                System.out.println("Erro ao rodas o algoritmo.");
-            }
-        } else if (entrada == 2) {
-            try {
-                rr.executar(pathFile);
-            } catch (Exception e) {
-                System.out.println("Erro ao rodas o algoritmo.");
-            }
-        }
-        ler.close();
+    System.out.println("[1] Processo não preemptivo");
+    System.out.println("[2] Processo preemptivo");
+    System.out.println("[3] Sair");
+    entrada = ler.nextInt();
+    String pathFile = ler.next();
+    System.out.println(pathFile);
+    if (entrada == 1) {
+      try {
+        sjf.executar(pathFile);
+      } catch (Exception e) {
+        System.out.println("Erro ao rodas o algoritmo.");
+      }
+    } else if (entrada == 2) {
+      try {
+        rr.executar(pathFile);
+      } catch (Exception e) {
+        System.out.println("Erro ao rodas o algoritmo.");
+      }
     }
+    ler.close();
+  }
 }
--- src/pcb/Pcb.java
+++ src/pcb/Pcb.java
@@ -2,66 +2,68 @@ package pcb;
 
 public class Pcb implements Comparable<Pcb> {
 
-	private Integer Id;
-	private Integer EstimatedTime;
-	private Integer quantum;
-	private String estado;
+  private Integer Id;
+  private Integer EstimatedTime;
+  private Integer quantum;
+  private String estado;
 
-	public Pcb() {
-		this.Id = 0;
-		this.EstimatedTime = 0;
-		this.quantum = 0;
-		this.estado = "";
-	}
-	// public Pcb(Integer Id=0,Integer EstimatedTime=0, Integer quantum,String estado) {
-	public Pcb(Integer Id,Integer EstimatedTime, Integer quantum,String estado) {
-		this.Id = Id;
-		this.EstimatedTime = EstimatedTime;
-		this.quantum = quantum;
-		this.estado = estado;
-	}
+  public Pcb() {
+    this.Id = 0;
+    this.EstimatedTime = 0;
+    this.quantum = 0;
+    this.estado = "";
+  }
 
-	public int compareTo(Pcb other) {
-		if (other.Id == this.Id && other.EstimatedTime == other.EstimatedTime)
-			return 1;
-		return 0;
-	}
+  // public Pcb(Integer Id=0,Integer EstimatedTime=0, Integer quantum,String
+  // estado) {
+  public Pcb(Integer Id, Integer EstimatedTime, Integer quantum, String estado) {
+    this.Id = Id;
+    this.EstimatedTime = EstimatedTime;
+    this.quantum = quantum;
+    this.estado = estado;
+  }
 
-	public void setId(Integer Id) {
-		this.Id = Id;
-	}
+  public int compareTo(Pcb other) {
+    if (other.Id == this.Id && other.EstimatedTime == other.EstimatedTime)
+      return 1;
+    return 0;
+  }
 
-	public void setEstimatedTime(Integer EstimatedTime) {
-		this.EstimatedTime = EstimatedTime;
-	}
+  public void setId(Integer Id) {
+    this.Id = Id;
+  }
 
-	public void setQuantum(Integer quantum) {
-		this.quantum = quantum;
-	}
+  public void setEstimatedTime(Integer EstimatedTime) {
+    this.EstimatedTime = EstimatedTime;
+  }
 
-	public Integer getId() {
-		return this.Id;
-	}
+  public void setQuantum(Integer quantum) {
+    this.quantum = quantum;
+  }
 
-	public Integer getEstimatedTime() {
-		return this.EstimatedTime;
-	}
+  public Integer getId() {
+    return this.Id;
+  }
 
-	public Integer getQuantum() {
-		return this.quantum;
-	}
+  public Integer getEstimatedTime() {
+    return this.EstimatedTime;
+  }
 
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
+  public Integer getQuantum() {
+    return this.quantum;
+  }
 
-	public String getEstado() {
-		return this.estado;
-	}
+  public void setEstado(String estado) {
+    this.estado = estado;
+  }
 
-	public String toString() {
-		return this.Id.toString();
+  public String getEstado() {
+    return this.estado;
+  }
 
-	}
+  public String toString() {
+    return this.Id.toString();
+
+  }
 
 }
